c23df8aafb7b7f542b80fa366c99d48500bfff3a
Petr <peachtrees2009@gmail.com>
2023-05-22 21:14:23 +0300

    expanded file capacity

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const {Schema, model, ObjectId}=require("mongoose");
 const User=new Schema({
     email:{type:String, required:true, unique:true},  //т.е. данное поле обязательно и должно быть уникальным
     password:{type:String, required:true},
-    diskSpace:{type:Number, default:1024*3*10},
+    diskSpace:{type:Number, default:1024*146484},  //150мб выделил
     usedSpace:{type: Number, default:0},
     avatar:{type:String},
     files:[{type:ObjectId, ref:'File'}] //т.е. мы связываем сущность пользователя с сущностью файлов. Тут у нас массив каждый объект которого имеет тип ObjectId и ссылается на сущность файла
